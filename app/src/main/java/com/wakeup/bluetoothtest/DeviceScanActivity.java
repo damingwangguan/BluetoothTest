e13eda2fd7433c27931714bd9a2e1186c806b568
Zhang Guangyao <zgy921229@foxmail.com>
2017-04-22 11:18:53 +0800

    搜索蓝牙

--- app/src/main/java/com/wakeup/bluetoothtest/DeviceScanActivity.java
+++ app/src/main/java/com/wakeup/bluetoothtest/DeviceScanActivity.java
@@ -1,22 +1,167 @@
 package com.wakeup.bluetoothtest;
 
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothManager;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+import adapter.LeDeviceListAdapter;
 
 /**
  * Created by liuqiong on 2017/4/21.
  */
 
 public class DeviceScanActivity extends AppCompatActivity {
+    private LeDeviceListAdapter mLeDeviceListAdapter;
+    private BluetoothAdapter bluetoothAdapter;
+    private static final int REQUEST_ENABLE_BT = 1;
+    private Context context;
+    private ListView listView;
+    private ArrayList<BluetoothDevice> bluetoothDevices;
+    private Handler mHandler;
+    private boolean mScanning;
+    private static final long SCAN_PERIOD = 10000;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_device);
-        getActionBar().setTitle("设备列表");
-        getActionBar().setDisplayHomeAsUpEnabled(true);
+        context=this;
+        mHandler = new Handler();
 
-        ListView listView = (ListView) findViewById(R.id.listview);
+        getSupportActionBar().setTitle("设备列表");
+        listView = (ListView) findViewById(R.id.listview);
+
+        BluetoothManager bluetoothManager = (BluetoothManager)getSystemService(Context.BLUETOOTH_SERVICE);
+        bluetoothAdapter = bluetoothManager.getAdapter();
+
+        if (bluetoothAdapter ==null) {
+            Toast.makeText(this,"不支持蓝牙",Toast.LENGTH_SHORT).show();
+            finish();
+            return;
+        }
+
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if (!bluetoothAdapter.isEnabled()) {
+            Intent intent = new Intent(bluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(intent,REQUEST_ENABLE_BT);
+        }
+
+        bluetoothDevices = new ArrayList<>();
+        mLeDeviceListAdapter = new LeDeviceListAdapter(context, bluetoothDevices);
+        listView.setAdapter(mLeDeviceListAdapter);
+
+        scanLeDevice(true);
+
+
+    }
+    private void scanLeDevice(final boolean enable) {
+        if (enable) {
+            // Stops scanning after a pre-defined scan period.
+            mHandler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    mScanning = false;
+                    bluetoothAdapter.stopLeScan(mLeScanCallback);
+                    invalidateOptionsMenu();
+                }
+            }, SCAN_PERIOD);
+
+            mScanning = true;
+            bluetoothAdapter.startLeScan(mLeScanCallback);
+        } else {
+            mScanning = false;
+            bluetoothAdapter.stopLeScan(mLeScanCallback);
+        }
+        invalidateOptionsMenu();
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        scanLeDevice(false);
+        mLeDeviceListAdapter.clear();
+    }
+
+    private BluetoothAdapter.LeScanCallback mLeScanCallback=new BluetoothAdapter.LeScanCallback(){
+
+        @Override
+        public void onLeScan(final BluetoothDevice bluetoothDevice, int i, byte[] bytes) {
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    mLeDeviceListAdapter.addDevice(bluetoothDevice);
+                    mLeDeviceListAdapter.notifyDataSetChanged();
+                }
+            });
+        }
+    };
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.device_list,menu);
+
+        if (!mScanning) {
+            menu.findItem(R.id.stop).setVisible(false);
+            menu.findItem(R.id.re_search).setVisible(true);
+            menu.findItem(R.id.menu_refresh).setActionView(null);
+        } else {
+            menu.findItem(R.id.stop).setVisible(true);
+            menu.findItem(R.id.re_search).setVisible(false);
+            menu.findItem(R.id.menu_refresh).setActionView(
+                    R.layout.actionbar_indeterminate_progress);
+        }
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.re_search:
+                mLeDeviceListAdapter.clear();
+                scanLeDevice(true);
+                break;
+
+            case R.id.sort:
+                break;
+            case R.id.stop:
+                scanLeDevice(false);
+
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode==REQUEST_ENABLE_BT&&resultCode== Activity.RESULT_CANCELED){
+            finish();
+            return;
+        }
     }
 }
