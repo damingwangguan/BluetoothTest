7d00e7b761a7fe5de917348b11a02afff766f13e
Zhang Guangyao <zgy921229@foxmail.com>
2017-05-06 16:20:53 +0800

    重搜

--- app/src/main/java/com/guangyao/bluetoothtest/adapter/LeDeviceListAdapter.java
+++ app/src/main/java/com/guangyao/bluetoothtest/adapter/LeDeviceListAdapter.java
@@ -52,8 +52,8 @@ public class LeDeviceListAdapter extends BaseAdapter {
     }
 
     public void clear() {
+        deviceBeens.clear();//先清除这个
         mLeDevices.clear();
-        deviceBeens.clear();
     }
 
     public void sort() {
